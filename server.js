9b994c7663c7f5a620e25c1638bfe00ab2368e05
Muhammad Rivki <Muhammad.Rivki@live.com>
2020-06-04 13:49:11 +0700

    inital commit

---
+++ server.js
@@ -0,0 +1,26 @@
+const { Telegraf } = require("telegraf")
+const functions = require("firebase-functions")
+
+const axios = require("axios")
+
+const bot = new Telegraf("1140311147:AAH-CmKM4rRzKYSD0BQwdsTTpxtQheju5U0")
+bot.start((ctx) => ctx.reply("Welcome"))
+bot.help((ctx) => ctx.reply("Send me a sticker"))
+bot.on("sticker", (ctx) => ctx.reply("👍"))
+bot.hears(/corona/gi, (ctx) => {
+  axios.get("https://covid19.mathdro.id/api/countries/idn").then((res) => {
+    const { confirmed, recovered, deaths } = res.data
+    ctx.reply(
+      `Hari ini yang teridentikasi terkena corona sejumlah ${confirmed.value}, yang sembuh ada sekitar ${recovered.value} dan yang meninggal sejumlah ${deaths.value}`
+    )
+  })
+})
+
+bot.hears(/hi bitch/gi, async (ctx) => {
+  console.log(ctx.get)
+  ctx.reply(`Hello @${ctx.message.from.username}`, {
+    reply_to_message_id: ctx.message.message_id,
+  })
+})
+
+bot.launch()
