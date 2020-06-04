df19c1d3b843b3bdaf435f80a1d8d9bbfc4fc1e8
Muhammad Rivki <Muhammad.Rivki@live.com>
2020-06-04 15:27:57 +0700

    add stickers

--- bot.js
+++ bot.js
@@ -1,17 +1,38 @@
 const { Telegraf } = require("telegraf")
-
 const axios = require("axios")
+const fs = require("fs")
+
+const stickers = require("./stickers")
 
 const bot = new Telegraf("1140311147:AAH-CmKM4rRzKYSD0BQwdsTTpxtQheju5U0")
+// const bot = new Telegraf("1111657351:AAHHgnVZdsLi-IpHY60SeNcZJVonJpLRIaI")
 bot.start((ctx) => ctx.reply("Welcome"))
 bot.help((ctx) => ctx.reply("Send me a stickersss"))
-bot.on("sticker", (ctx) => ctx.reply("👍"))
+bot.on("sticker", (ctx) => {
+  const uniqueId = ctx.message.sticker.file_unique_id
+  if (uniqueId === stickers["assalamualaikum"].uniqueId) {
+    ctx.replyWithSticker(stickers["waalaikumsalam"].id)
+  }
+})
+
+bot.hears(/sticker/gi, (ctx) =>
+  ctx.replyWithSticker(
+    "CAACAgIAAx0CSf7kgwACClpe2KTuE-x7MK-dXllhpwyZNKXucQAConcBAAFji0YMmP33swABVmfRGgQ"
+  )
+)
 bot.hears(/corona/gi, (ctx) => {
   axios.get("https://covid19.mathdro.id/api/countries/idn").then((res) => {
     const { confirmed, recovered, deaths } = res.data
     ctx.reply(
-      `Hari ini yang teridentikasi terkena corona sejumlah ${confirmed.value}, yang sembuh ada sekitar ${recovered.value} dan yang meninggal sejumlah ${deaths.value}`
+      `Hari ini yang teridentikasi terkena corona sejumlah *${confirmed.value}*, yang sembuh ada sekitar *${recovered.value}* dan yang meninggal sejumlah *${deaths.value}* `,
+      { parse_mode: "MarkdownV2" }
     )
+  })
+})
+
+bot.hears(/hi ariya/gi, (ctx) => {
+  ctx.reply(`hi @${ctx.message.from.username} bitch!!`, {
+    reply_to_message_id: ctx.message.message_id,
   })
 })
 
@@ -20,6 +41,20 @@ bot.hears(/hi bitch/gi, async (ctx) => {
   ctx.reply(`Hello @${ctx.message.from.username}`, {
     reply_to_message_id: ctx.message.message_id,
   })
+})
+
+bot.hears(/itungin/gi, (ctx) => {
+  const text = ctx.message.text
+  const isMultiple =
+    ["*", "x", "kali"].filter((x) => text.indexOf(x)).length > 1
+  const splitText = text.split(" ")
+  const arrNumber = splitText.filter(Number).map(Number)
+
+  let result = 0
+  if (isMultiple) {
+    result = arrNumber.reduce((prev, next) => prev * next, 1)
+  }
+  ctx.reply(`hasilnya adalah ${result}`)
 })
 
 bot.launch()
---
+++ stickers.js
@@ -0,0 +1,57 @@
+module.exports = {
+  assalamualaikum: {
+    id:
+      "CAACAgUAAx0CSf7kgwACCqJe2K9xynP_1e052vkYf2M1nrAe5AACFwADrBblDGeadZqkGFDzGgQ",
+    uniqueId: "AgADFwADrBblDA",
+  },
+  waalaikumsalam: {
+    id:
+      "CAACAgUAAx0CSf7kgwACCqRe2K-T_xdAhE84NuxyJI2C9kyp1wACGAADrBblDF8ZkAoppMAyGgQ",
+    uniqueId: "AgADGAADrBblDA",
+  },
+  kasihPoop: {
+    id:
+      "CAACAgUAAx0CSf7kgwACCqVe2K-lsi_B7MiPlEEzIPkl2Y9BHAACIQADrBblDHOAmwABeKiZshoE",
+    uniqueId: "AgADIQADrBblDA",
+  },
+  cus: {
+    id:
+      "CAACAgUAAx0CSf7kgwACCqZe2K-4QhkDw0MhOCGxthIm-kc7EwACGQADrBblDCSeHW6Bw8v1GgQ",
+    uniqueId: "AgADGQADrBblDA",
+  },
+  fucek: {
+    id:
+      "CAACAgUAAx0CSf7kgwACCqde2K_NIfYMewYuO8m5VHaiSSE_qgACFAADrBblDHdWrJNLgM0dGgQ",
+    uniqueId: "AgADFAADrBblDA",
+  },
+  jilat: {
+    id:
+      "CAACAgUAAx0CSf7kgwACCqhe2K_hs4EvBhEDnCxXAz4klcxamAACCwADrBblDAKmOUFqppqnGgQ",
+    uniqueId: "AgADCwADrBblDA",
+  },
+  chills: {
+    id:
+      "CAACAgUAAx0CSf7kgwACCqle2K_w04h-sZkEUlMaoGIwh3BN0QACDAADrBblDOSFc5JmvhAcGgQ",
+    uniqueId: "AgADDAADrBblDA",
+  },
+  tai: {
+    id:
+      "CAACAgUAAx0CSf7kgwACCqpe2LAIaf1Y2rmx2stb8Pk3LzlyqAACEAADrBblDI7tHAiOGwbmGgQ",
+    uniqueId: "AgADEAADrBblDA",
+  },
+  bully: {
+    id:
+      "CAACAgUAAx0CSf7kgwACCqte2LAW6wIDpDWYmx8wKS1B6X3u7gACHQADrBblDL_dFbXe1KVaGgQ",
+    uniqueId: "AgADHQADrBblDA",
+  },
+  iyuwh: {
+    id:
+      "CAACAgUAAx0CSf7kgwACCqxe2LAkyT-FzO2r9jZVKo910AfiVQACHwADrBblDAqn_Kk4OW4BGgQ",
+    uniqueId: "AgADHwADrBblDA",
+  },
+  zzz: {
+    id:
+      "CAACAgUAAx0CSf7kgwACCq1e2LAwXsdLIeXLlxMzsl7R1fjJ7AACIAADrBblDJjhLcdDpwusGgQ",
+    uniqueId: "AgADIAADrBblDA",
+  },
+}
