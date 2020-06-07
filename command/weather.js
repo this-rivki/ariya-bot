7524f3e907e98c09efb1450b94decaf5f085b044
Muhammad Rivki <Muhammad.Rivki@live.com>
2020-06-07 13:00:54 +0700

    update command for weather

--- command/weather.js
+++ command/weather.js
@@ -1,3 +1,4 @@
+const Extra = require('telegraf/extra')
 const Markup = require('telegraf/markup')
 const axios = require('axios')
 const K2C = require('kelvin-to-celsius')
@@ -10,22 +11,62 @@ module.exports = (bot) => {
     const city = ctx.message.text
       .replace(/\/cuaca.*bot|\/cuaca/gi, '')
       .replace(/\s/g, '')
-    console.log(city)
-    if (!city) return ctx.reply('kota mana begoo?!? bego banget dah!!')
+    if (!city) {
+      return ctx.reply(
+        'kota mana begoo?!? bego banget dah!!',
+        Markup.keyboard([
+          ['🌡 Jakarta', '🌡 Bandung'], // Row1 with 2 buttons
+          ['🌡 Cirebon', '🌡 Sukabumi'], // Row2 with 2 buttons
+        ])
+          .oneTime()
+          .resize()
+          .extra()
+      )
+    }
 
     axios.get(API_URL(city)).then((res) => {
       const locationData = res.data.list[0]
-      ctx.reply(
-        `
-Nihh, info dari bitch ya, gue ramal di *${city} itu ${
-          locationData.weather[0].description
-        }*.
-untuk suhunya sendiri yang *paling rendah itu ${K2C(
-          locationData.main.temp_min
-        )}°C* terus *maksimalnya ${K2C(locationData.main.temp_max)}°C*.
-terus-terus *kecepatan anginnya ${locationData.wind.speed} m/s*
+      ctx.replyWithPhoto(
+        {
+          url: `https://openweathermap.org/img/wn/${locationData.weather[0].icon}@2x.png`,
+        },
+
+        Extra.load({
+          caption: `
+Nihh, info dari bitch ya, gue ramal di ${city} itu ${
+            locationData.weather[0].description
+          }.
+untuk suhunya sendiri yang paling rendah itu ${K2C(
+            locationData.main.temp_min
+          )}°C terus maksimalnya ${K2C(locationData.main.temp_max)}°C.
+terus-terus kecepatan anginnya ${locationData.wind.speed} m/s
 			`,
-        { parse_mode: 'Markdown' }
+        })
+      )
+    })
+  })
+
+  bot.hears(/[cuaca|🌡].*[jakarta|bandung|cirebon|sukabumi]/gi, (ctx) => {
+    const city = ctx.message.text.split(/\s/gi)[1]
+
+    axios.get(API_URL(city)).then((res) => {
+      const locationData = res.data.list[0]
+      ctx.replyWithPhoto(
+        {
+          url: `https://openweathermap.org/img/wn/${locationData.weather[0].icon}@2x.png`,
+        },
+
+        Extra.load({
+          caption: `
+Nihh, info dari bitch ya, gue ramal di ${city} itu ${
+            locationData.weather[0].description
+          }.
+untuk suhunya sendiri yang paling rendah itu ${K2C(
+            locationData.main.temp_min
+          )}°C terus maksimalnya ${K2C(locationData.main.temp_max)}°C.
+terus-terus kecepatan anginnya ${locationData.wind.speed} m/s
+			`,
+        })
       )
     })
   })
